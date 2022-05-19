f2e8a0c93be135821b59f269039ba26bd974fd89
vijay kokatnur <vijay.kokatnur@oracle.com>
2022-05-19 14:50:30 +0530

    required_providers

--- Terraform/oci-vcn/provider.tf
+++ Terraform/oci-vcn/provider.tf
@@ -2,3 +2,13 @@ provider oci {
 	auth = "InstancePrincipal"
 	region = var.region
 }
+
+terraform {
+  required_providers {
+    mycloud = {
+      source  = "oracle/oci"
+      version = "~> 1.0"
+    }
+  }
+}
+
