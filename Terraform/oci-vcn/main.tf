f74f8581635184c35a3359b4d7ae5ef1ec0c957c
vijay kokatnur <vijay.kokatnur@oracle.com>
2022-05-31 11:20:16 +0530

    vcn main module

--- Terraform/oci-vcn/main.tf
+++ Terraform/oci-vcn/main.tf
@@ -2,6 +2,17 @@
 #Latest comments on oci vcn resources tf script
 
 
+resource "vcn"{
+    source = "./modules/vcn" 
+
+    vcn_name = var.vcn_name
+    vcn_dns_label = var.vcn_dns_label
+    vcn_cidr_block = var.vcn_cidr_block
+
+    subnet_cidr_block = var.subnet_cidr_block
+    subnet_name = var.subnet_name
+}
+
 resource "oci_core_internet_gateway" "ig" {
   compartment_id = var.compartment_id
   display_name   = var.label_prefix == "none" ? var.internet_gateway_display_name : "${var.label_prefix}-${var.internet_gateway_display_name}"
--- Terraform/oci-vcn/terraform.tfvars
+++ Terraform/oci-vcn/terraform.tfvars
@@ -3,11 +3,9 @@ tenancy_id     = "ocid1.tenancy.oc1..aaaaaaaaxn3r3dh3lh5n5hyo6tkbtmwjvr4wzlahs3u
 user_id        = "ocid1.user.oc1..aaaaaaaakpa3klyfbdwprdej3tvov742attpkeb5rv6xp3y2poibwnwqn3bq"
 api_fingerprint      = "05:61:63:8b:e5:4f:1c:a1:a9:58:85:64:d7:3f:0e:37"
 api_private_key_path = "/home/opc/.oci/user_api_private.pem"
-
 region = "us-ashburn-1"
 
 compartment_id = "ocid1.compartment.oc1..aaaaaaaawrvaz3qenxkroaq6zxnqfndsqvm4nylcxlnhpyp7mrbbo2pzhboa" 
-
 label_prefix = "none"
 
 vcn_name = "prac-vcn"
