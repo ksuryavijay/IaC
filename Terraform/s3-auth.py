00b01641e531675d9d048cb2146dc19dec78ad11
Vijay Kokatnur <vijay.kokatnur@oracle.com>
2022-11-17 13:05:08 +0530

    instance principal authentication

--- Terraform/s3-auth.py
+++ Terraform/s3-auth.py
@@ -14,13 +14,16 @@ from pathlib import Path
 #userhome = sys.argv[1]
 #print('userhome'+ userhome)
 print ("str(Path.home()) "+ str(Path.home()))
+"""
+#Authentication using config file ~/.oci/config 
+  config = oci.config.from_file(file_location=str(Path.home())+'/.oci/config', profile_name='DEFAULT')
+  secrets_client = oci.secrets.SecretsClient(config)
+"""
+#Authentication using Instance Principal
+signer = oci.auth.signers.InstancePrincipalsSecurityTokenSigner()
 
-#config = oci.config.from_file(file_location=userhome+'/.oci/config', profile_name='DEFAULT')
-config = oci.config.from_file(file_location=str(Path.home())+'/.oci/config', profile_name='DEFAULT')
-
-secrets_client = oci.secrets.SecretsClient(config)
+secrets_client = oci.secrets.SecretsClient(config, signer)
 result = uuid.uuid4()
-#secret_id = "ocid1.vaultsecret.oc1.me-jeddah-1.amaaaaaar3ercziar3pmfe55msrj6oq4xgxsoxr5tpzr3ealljy256opzrea"
 secret_id = "ocid1.vaultsecret.oc1.iad.amaaaaaazjgvoqyafuyhoiy35cosfhlc475mbihrim2imlmw7dlxrdaewhda"
 
 get_secret_bundle_response = secrets_client.get_secret_bundle(
